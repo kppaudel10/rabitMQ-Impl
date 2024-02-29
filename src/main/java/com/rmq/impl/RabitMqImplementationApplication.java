83f378d8f8a2cc69272418c795c0e277d9251447
Kul paudel <kppaudel56@gmail.com>
2024-02-29 10:26:10 +0545

    feat(rabitMQ-impl): initial project setup

---
+++ src/main/java/com/rmq/impl/RabitMqImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.rmq.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitMqImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabitMqImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rmq/impl/RabitMqImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmq.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitMqImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
